a788176bfff23b9f0b0b095b153d90d903e47d80
Karen9041 <karen010401@gmail.com>
2021-03-11 12:22:32 +0800

    fix menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 export default function Menu() {
     return (
         <div className="menu row flex-column">
-            <p className="text-light opacity-3 pt-3">Tableware</p>
+            <p className="text-light opacity-3 pt-3" >Tableware</p>
             <p className="text-light opacity-3 pt-3">Cookware</p>
             <p className="text-light opacity-3 pt-3">Home accessories</p>
             <p className="text-light opacity-3 pt-3">Lighting</p>
